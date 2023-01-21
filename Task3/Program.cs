d7f3465a9c85156d562aee18463904394fc25ca2
Dmitry <mangosovdima@mail.ru>
2023-01-21 21:23:40 +0300

    Added task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine());
+while(number>=5)
+{
+Console.Write("Введите номер четверти: ");
+ number = int.Parse(Console.ReadLine());
+}
+if(number == 1) Console.WriteLine("Первая четверть, координаты х>0 и y>0, т.е х до +бесконечности и y до +бесконечности");
+if(number == 2) Console.WriteLine("Вторая четверть, координаты х<0 и y>0, т.е х до -бесконечности и y до +бесконечности");
+if(number == 3) Console.WriteLine("Третья четверть, координаты х<0 и y<0, т.е х до -бесконечности и y до -бесконечности");
+if(number == 4) Console.WriteLine("Четвёртая четверть, координаты х>0 и y<0, т.е х до +бесконечности и y до -бесконечности");
